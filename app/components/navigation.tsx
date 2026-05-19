8467fecf9eadb9fc465fe4964901ba440ea047ad
Alvis97 <101591973+Alvis97@users.noreply.github.com>
2026-05-19 12:22:04 +0100

    Added hamburger menu

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,11 +1,22 @@
 "use client"
 
-import { Moon, Sun } from 'lucide-react'
+import { Menu, Moon, Sun } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 
 function Navigation() {
     const [lightmode, setLightmode] = useState(false)
     const [active, setActive] = useState("Home")
+    const [mobileNav, setMobileNav] = useState(false)
+    const [modalOpen, setModalOpen] = useState(false)
+
+    useEffect(() => {
+         function checkScreen() {
+        setMobileNav(window.innerWidth >= 768)
+    } 
+    checkScreen()
+     window.addEventListener("resize", checkScreen)
+    return () => window.removeEventListener("resize", checkScreen)
+    }, [])   
 
  useEffect(() => {
     if (lightmode) {
@@ -17,7 +28,56 @@ function Navigation() {
 
   return (
     <div className='flex w-full fixed z-1 justify-between backdrop-blur-md bg-white/0.5 p-3'>
-        <img className='h-8 w-8' src="/heart.svg" alt="" />
+
+    {!mobileNav && (
+        <>
+          <img className='h-8 w-8' src="/heart.svg" alt="" />
+          <div>
+            <button 
+            onClick={() => setModalOpen(!modalOpen)}
+            className={`transition-transform duration-300 ${modalOpen ? "rotate-90" : "-rotate-0"} cursor-pointer `}
+            >
+                <Menu/>
+            </button>
+            { modalOpen && (
+            <div className='mobile-menu fixed flex flex-col justify-center items-end p-10 gap-5 top-13 left-0 h-screen w-screen'>
+                  <a 
+                    onClick={() => {setActive("Home"); setModalOpen(false)}}
+                    href="#landingSection"
+                    className='a-link'
+                    >
+                        Home
+                    </a>
+                    <a 
+                    onClick={() => {setActive("About"); setModalOpen(false)}}
+                    href="#about"
+                    className='a-link'
+                    >
+                        About
+                    </a>
+                    <a 
+                    onClick={() => {setActive("Projects"); setModalOpen(false)}}
+                    href="#projects"
+                    className='a-link'
+                    >
+                        Projects
+                    </a>
+                <button onClick={() => setLightmode(!lightmode)}>
+                {lightmode ? ( <Sun/> ) : ( <Moon/> )}
+        </button>
+               
+            </div>
+            )}
+        
+          </div>
+        </>
+    )}
+
+
+
+    {mobileNav && (
+        <>
+         <img className='h-8 w-8' src="/heart.svg" alt="" />
 
         <div className='flex gap-15'>
             <a 
@@ -43,6 +103,9 @@ function Navigation() {
         <button onClick={() => setLightmode(!lightmode)}>
             {lightmode ? ( <Sun/> ) : ( <Moon/> )}
         </button>
+        </>
+    )}
+       
     </div>
   )
 }
