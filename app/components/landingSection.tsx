b5a4ca1a60613a562d285398e6905612e80af472
Alvis97 <101591973+Alvis97@users.noreply.github.com>
2026-05-20 11:18:12 +0100

    updated landingpage responsivness

--- app/components/landingSection.tsx
+++ app/components/landingSection.tsx
@@ -16,14 +16,14 @@ function LandingSection() {
   return (
 <div className='h-screen w-screen flex items-center justify-center relative'>
 <div className='flex flex-col gap-0 items-center justify-center w-full md:relative md:m-auto md:w-[80vw] md:max-w-[1300px] md:h-[100%]'>
-        <div className='relative flex w-[65%] h-[120px] mx-auto md:absolute md:my-auto md:left-[5%] md:my-auto md:w-[95%] md:h-[65%] lg:h-[85%] '>
+        <div className='relative flex w-[65%] h-[120px] mx-auto md:absolute md:my-auto md:left-[5%] md:my-auto md:w-[95%] md:h-[500px] lg:h-[85%] '>
            <img className='absolute heart-bounce rotate-[-10deg] h-[100%] w-[28%] lg:left-[0%] md:w-[25%] md:h-[45%]' style={{animationDelay: '0s'}} src="/heart.svg" alt="heart"/>
            <img className='absolute heart-bounce rotate-[-130deg] h-[100%] w-[28%] left-[23%] bottom-3 md:left-[27%] md:top-0 md:w-[25%] md:h-[45%]' style={{animationDelay: '0.3s'}} src="/heart.svg" alt="heart"/>
            <img className='absolute heart-bounce rotate-[170deg] h-[100%] w-[28%] right-[23%] bottom-3 md:left-[56%] md:top-27 md:w-[25%] md:h-[45%]' style={{animationDelay: '0.6s'}} src="/heart.svg" alt="heart"/>
            <img className='absolute heart-bounce rotate-[10deg] h-[100%] w-[28%] right-0 md:bottom-10 md:left-[75%] md:w-[25%] md:h-[45%]' style={{animationDelay: '0.9s'}} src="/heart.svg" alt="heart"/>
         </div>
 
-        <div className='text-center w-[90%] mx-auto md:text-left md:absolute md:bottom-15 md:left-0 md:w-100 lg:w-130'>
+        <div className='text-center w-[90%] mx-auto md:text-left md:absolute md:bottom-55 md:left-0 md:w-100 lg:w-130 lg:bottom-15'>
             <p className='font-[var(--font-onest)] text-[var(--accent)] text-sm md:text-sm lg:text-sm'>
                 ALVA JONSSON
                 </p>
